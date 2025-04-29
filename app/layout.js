29ae6526b486ccf859e597066b31d2da9b79f32e
Liza172 <lizarakshit@gmail.com>
2025-04-29 20:32:16 +0530

    account creation error

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 const inter = Inter({subsets: ["latin"]});
+
 
 export const metadata = {
   title: "CashCaddy",
@@ -17,6 +19,7 @@ export default function RootLayout({ children }) {
           className={`${inter.className}`} >
           <Header></Header>
           <main className="min-h-screen">{children}</main>
+          <Toaster richColors/>
           {/*Footer*/}
           <footer className="bg-blue-50">
             <div className="container mx-auto px-4 text-center text-grey-600">
