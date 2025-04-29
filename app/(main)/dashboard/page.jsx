a2c6e761214eaeab91f725d134718f129b5dfadc
Liza172 <lizarakshit@gmail.com>
2025-04-29 17:34:22 +0530

    dashboard ready

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DashboardPage() {
+  return (
+    <div className='px-5'>
+      <h1 className='text-6xl font-bold gradient-title mb-5'>Dashboard</h1>
+    </div>
+  )
+}
+
+export default DashboardPage
